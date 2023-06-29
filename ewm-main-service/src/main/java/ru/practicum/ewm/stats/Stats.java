1936db3d5eb9b58b82299b6ec6efedd7513a7345
tonasd <tonasd@ya.ru>
2023-06-30 00:35:55 +0300

    dirty working version, all tests are passed

--- ewm-main-service/src/main/java/ru/practicum/ewm/stats/Stats.java
+++ ewm-main-service/src/main/java/ru/practicum/ewm/stats/Stats.java
@@ -3,6 +3,7 @@ package ru.practicum.ewm.stats;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import ru.practicum.ewm.stats.client.StatsClient;
+import ru.practicum.stats.dto.ViewDto;
 
 import javax.servlet.http.HttpServletRequest;
 import java.net.InetAddress;
@@ -10,12 +11,13 @@ import java.net.URI;
 import java.net.UnknownHostException;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
 public class Stats {
     private static final String APP_NAME = "explore-with-me";
-    private static final URI EVENT_URI = URI.create("/events/");
+    private static final String EVENT_URI = "/events/";
     private static final LocalDateTime BEFORE_ALL = LocalDateTime.of(1970, 1, 1, 0, 0);
     private static final LocalDateTime AFTER_ALL = LocalDateTime.of(2970, 1, 1, 0, 0);
     private final StatsClient statsClient;
@@ -30,12 +32,13 @@ public class Stats {
     }
 
     public long getViewsForEvent(long eventId) {
-        statsClient.getStats(
+        Optional<ViewDto> dto = statsClient.getStats(
                 BEFORE_ALL,
                 AFTER_ALL,
                 true,
-                List.of(EVENT_URI.resolve(Long.toString(eventId)))
-        );
-        return 0;
+                List.of(URI.create(EVENT_URI + eventId))
+        ).stream().findFirst();
+
+        return dto.map(ViewDto::getHits).orElse(0L);
     }
 }
